1a8351aaa68746c4ee07080a57bd3546731bcd09
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-28 23:08:29 +0500

    done with ejs

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const hbs = require("express-handlebars");
 
 const app = express();
 
-app.engine("hbs", hbs.engine());
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
