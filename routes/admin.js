7234bdaf5821ebf1bc813ff26763696bc73ff3e2
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-29 19:29:30 +0500

    Refactoring App

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const products = [];
 
-router.get("/add-product", productsController.getAddProduct);
+//   /admin/add-product -> GET
+router.get("/add-product", adminController.getAddProduct);
 
-router.post("/add-product", productsController.postAddProduct);
+//   /admin/add-product -> POST
+router.post("/add-product", adminController.postAddProduct);
+
+//   /admin/products -> GET
+router.get("/products", adminController.getProducts);
 
 module.exports = router;
