fcd593c68857a790af6426712c9426d67d0a4a61
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-08-29 17:38:59 +0500

    done with delete product

--- models/product.js
+++ models/product.js
@@ -130,7 +130,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -172,6 +172,17 @@ class Product {
       .next()
       .then((product) => {
         return product;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getdb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("deleted!");
       })
       .catch((err) => console.log(err));
   }
