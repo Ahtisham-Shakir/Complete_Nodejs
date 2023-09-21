896c85a71208e288a96967d866f8c52a316ed114
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-09-21 19:04:34 +0500

    added routes protect middleware

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
-router.get("/cart", shopController.getCart);
-router.post("/cart", shopController.postCart);
-router.post("/cart-delete-item", shopController.postDeleteCartItem);
-router.get("/orders", shopController.getOrders);
-router.post("/create-order", shopController.postOrder);
+router.get("/cart", isAuth, shopController.getCart);
+router.post("/cart", isAuth, shopController.postCart);
+router.post("/cart-delete-item", isAuth, shopController.postDeleteCartItem);
+router.get("/orders", isAuth, shopController.getOrders);
+router.post("/create-order", isAuth, shopController.postOrder);
 
 module.exports = router;
